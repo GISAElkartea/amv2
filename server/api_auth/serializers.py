8c2ea527864d6aca472ee9493db4328eff636a2a
Unai Zalakain <unai@gisa-elkartea.org>
2014-09-27 23:41:39 +0100

    Confirmation sending registration view and confirmation checking view

--- server/api_auth/serializers.py
+++ server/api_auth/serializers.py
@@ -6,7 +6,8 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
+        fields = ('email', 'favorite_radio_shows', 'favorite_news_shows')
 
-    def save_object(self, obj):
+    def save_object(self, obj, **kwargs):
         obj.set_password(self.init_data['password'])
-        obj.save()
+        obj.save(**kwargs)
--- server/api_auth/urls.py
+++ server/api_auth/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
-from .views import UserView
+from .views import UserView, ConfirmationView
 
 
 urlpatterns = patterns(
     '',
     url(r'^token/', 'rest_framework_jwt.views.obtain_jwt_token', name='auth'),
     url(r'^user/', UserView.as_view(), name='user'),
+    url(r'^reset/', ConfirmationView.as_view(), name='password_reset'),
 )
