4255fa6efc423fe08f860e11941ed2ab8155dec0
Unai Zalakain <unai@gisa-elkartea.org>
2014-09-29 17:59:56 +0100

    CORS enabling settings

--- client/app/app.js
+++ client/app/app.js
@@ -15,8 +15,14 @@ var App = angular.module('app', [
     function($locationProvider, $urlRouterProvider, $httpProvider, RestangularProvider, baseUrl) {
 	// Server side support is needed
 	$locationProvider.html5Mode(false);
+        // Set base url for the API
         RestangularProvider.setBaseUrl(baseUrl);
+        // Send JWT headers with every request
         $httpProvider.interceptors.push('AuthInterceptor');
+        // Enable CORS
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        // Default route
 	$urlRouterProvider.otherwise('/radio');
     }
 );
