30c597ba8ee5b917afcaf1e5f57e734527b316eb
Unai Zalakain <unai@gisa-elkartea.org>
2014-10-11 23:43:50 +0100

    angular-decorate no longer needed

--- client/config.js
+++ client/config.js
@@ -31,7 +31,6 @@ exports.config = {
 			    case 'bower_components/angular-sanitize/angular-sanitize.js':
 			    case 'bower_components/angular-ui-router/release/angular-ui-router.js':
 			    case 'bower_components/angular-local-storage/angular-local-storage.js':
-                            case 'bower_components/angular-decorate/angular-decorate.js':
                             case 'bower_components/ngFitText/src/ng-FitText.js':
 
 			    return true;
