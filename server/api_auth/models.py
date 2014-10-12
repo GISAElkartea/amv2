e1776b197f70319fe8d4442236fbff0fd623ff9e
Unai Zalakain <unai@gisa-elkartea.org>
2014-10-12 14:44:28 +0100

    Accept null radio and news favorite shows

--- server/api_auth/models.py
+++ server/api_auth/models.py
@@ -4,5 +4,5 @@ from custom_user.models import AbstractEmailUser
 
 
 class APIUser(AbstractEmailUser):
-    favorite_radio_shows = models.ManyToManyField('radio.RadioShow')
-    favorite_news_shows = models.ManyToManyField('radio.NewsShow')
+    favorite_radio_shows = models.ManyToManyField('radio.RadioShow', null=True)
+    favorite_news_shows = models.ManyToManyField('radio.NewsShow', null=True)
