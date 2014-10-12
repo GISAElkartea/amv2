6f850370b9c3156a10251f652f0fe0480b185aad
Unai Zalakain <unai@gisa-elkartea.org>
2014-10-12 14:44:12 +0100

    Explicitly define user serializer fields and make password writeonly

--- server/api_auth/serializers.py
+++ server/api_auth/serializers.py
@@ -6,7 +6,9 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('email', 'favorite_radio_shows', 'favorite_news_shows')
+        fields = ('email', 'password',
+                  'favorite_news_shows', 'favorite_radio_shows')
+        write_only_fields = ('password',)
 
     def save_object(self, obj, **kwargs):
         obj.set_password(self.init_data['password'])
