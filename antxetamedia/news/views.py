dc3772a4f7caa41300c1c3b068abbb89a3b9573e
Unai Zalakain <unai@gisa-elkartea.org>
2015-06-26 10:58:18 +0200

    Fix NewsPodcast filter view

--- antxetamedia/news/views.py
+++ antxetamedia/news/views.py
@@ -1,5 +1,5 @@
-from django.db.models import Q
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from .models import NewsPodcast, NewsShow, NewsCategory
 from .forms import NewsPodcastFilter
@@ -12,28 +12,28 @@ class NewsPodcastMixin(object):
 
 class NewsPodcastList(NewsPodcastMixin, ListView):
     def get_queryset(self):
+        query = Q()
         shows = self.request.GET.getlist('show')
         categories = self.request.GET.getlist('category')
-        query = Q(show__slug__in=shows) & Q(categories__slug__in=categories)
+        if shows or categories:
+            query = Q(show__slug__in=shows) | Q(categories__slug__in=categories)
         return super(NewsPodcastList, self).get_queryset().filter(query)
 
-    def get_initial_shows(self):
-        qs = NewsShow.objects.all()
-        selected = self.request.GET.getlist('show')
-        qs = qs.filter(slug__in=selected) if selected else qs
-        return qs.values_list('slug', flat=True)
-
-    def get_initial_categories(self):
-        qs = NewsCategory.objects.all()
-        selected = self.request.GET.getlist('category')
-        qs = qs.filter(slug__in=selected) if selected else qs
-        return qs.values_list('slug', flat=True)
+    def get_initial_data(self):
+        shows = NewsShow.objects.all()
+        categories = NewsCategory.objects.all()
+        selected_shows = self.request.GET.getlist('show')
+        selected_categories = self.request.GET.getlist('category')
+        if selected_shows or selected_categories:
+            shows = shows.filter(slug__in=selected_shows)
+            categories = categories.filter(slug__in=selected_categories)
+        return {
+            'show': shows.values_list('slug', flat=True),
+            'category': categories.values_list('slug', flat=True),
+        }
 
     def get_context_data(self, *args, **kwargs):
-        kwargs['form'] = NewsPodcastFilter(initial={
-            'show': self.get_initial_shows(),
-            'category': self.get_initial_categories(),
-        })
+        kwargs['form'] = NewsPodcastFilter(initial=self.get_initial_data())
         return super(NewsPodcastList, self).get_context_data(*args, **kwargs)
 
 
