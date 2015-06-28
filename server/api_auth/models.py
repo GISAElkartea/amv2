06b00c077800da9d163b965c764c4af7150afca1
Unai Zalakain <unai@gisa-elkartea.org>
2015-06-28 21:42:33 +0200

    Delete old server side code

--- server/api_auth/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from custom_user.models import AbstractEmailUser
-
-
-class APIUser(AbstractEmailUser):
-    favorite_radio_shows = models.ManyToManyField('radio.RadioShow', null=True)
-    favorite_news_shows = models.ManyToManyField('radio.NewsShow', null=True)
--- server/radio/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url, include
-
-urlpatterns = patterns(
-    '',
-    url(r'^', include('radio.shows.urls')),
-    url(r'^playlists', include('radio.playlists.urls')),
-)
