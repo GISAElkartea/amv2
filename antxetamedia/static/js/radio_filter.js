fadd42c3be8e115de4768768ae191c8a5ff10026
Unai Zalakain <unai@gisa-elkartea.org>
2015-07-09 12:39:02 +0200

    Fixed #37: radio show list JS filter

---
+++ antxetamedia/static/js/radio_filter.js
@@ -0,0 +1,35 @@
+function toggleLabel() {
+  this.parentElement.classList.toggle('checked', this.checked);
+}
+
+function filterShows() {
+  var filter = this.name,
+      value = this.value,
+      checked = this.checked;
+  var selector = '#radioShows li.show[data-{filter}="{value}"]',
+      tag = 'data-{filter}-is-hidden';
+  selector = selector.replace('{filter}', filter);
+  selector = selector.replace('{value}', value);
+  tag = tag.replace('{filter}', filter);
+  var shows = document.querySelectorAll(selector);
+  Array.forEach(shows, function(show) {
+    show.setAttribute(tag, !checked);
+    if (show.getAttribute('data-producer-is-hidden') === "true" &&
+        show.getAttribute('data-category-is-hidden') === "true") {
+      show.style.display = 'none';
+    } else {
+      show.style.display = 'block';
+    }
+  });
+}
+
+
+var checkboxes = document.querySelectorAll('form li input');
+Array.forEach(checkboxes, function(checkbox) {
+  toggleLabel.apply(checkbox);
+  filterShows.apply(checkbox);
+  checkbox.onclick = function(event) {
+    toggleLabel.apply(this);
+    filterShows.apply(this);
+  };
+});
