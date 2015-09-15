e2369640d9227d96dbf8d9d234c8fc351a74c96d
Unai Zalakain <unai@gisa-elkartea.org>
2015-09-15 23:54:55 +0200

    Fixed #147 -- The active tab has an `active` class
    @iunetrecet the active tab has now always an `active` class.

--- antxetamedia/static/js/tabs.js
+++ antxetamedia/static/js/tabs.js
@@ -1,5 +1,10 @@
 (function() {
   function hideAllTabContent() {
+    var tabs = document.querySelectorAll('.tab');
+    Array.forEach(tabs, function(tab) {
+      tab.classList.remove('active');
+    });
+
     var tabContents = document.querySelectorAll('.tabContent');
     Array.forEach(tabContents, function(content) {
       content.style.display = 'none';
@@ -7,6 +12,7 @@
   }
 
   function showTabContent() {
+    this.classList.add('active');
     var tab = this.getAttribute('data-tab');
     var tabContent = document.querySelector('.tabContent[data-tab="{tab}"]'.replace('{tab}', tab));
     tabContent.style.display = 'block';
