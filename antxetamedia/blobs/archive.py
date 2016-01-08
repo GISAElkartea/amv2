552910651f574926d9561f3963722edf47e0a28b
Unai Zalakain <unai@gisa-elkartea.org>
2016-01-08 23:09:46 +0100

    Sync with archive's S3 API when podcasts are changed too

---
+++ antxetamedia/blobs/archive.py
@@ -0,0 +1,50 @@
+try:
+    from urllib.parse import quote
+except ImportError:
+    from urllib import quote
+
+from boto.s3.connection import S3Connection, OrdinaryCallingFormat
+from boto.s3.bucket import Bucket
+
+
+class ArchiveS3(object):
+    def __init__(self, account):
+        self.account = account
+        # No real connection is maintained open
+        self.connection = S3Connection(self.account.username, self.account.password, is_secure=False,
+                                       host='s3.us.archive.org', calling_format=OrdinaryCallingFormat())
+
+    def get_bucket_name(self, podcast):
+        return '{prefix}-{show}-{podcast}'.format(prefix=self.account.username.lower(),
+                                                  show=podcast.show.slug.lower(),
+                                                  podcast=podcast.slug.lower())
+
+    def get_bucket_headers(self, podcast, update=False):
+        headers = getattr(podcast, 'metadata', {})
+        headers = {key: 'uri({})'.format(value) for key, value in headers.items()}
+        if update:
+            headers['x-archive-ignore-preexisting-bucket'] = 1
+        return headers
+
+    def create_or_update_bucket(self, podcast):
+        bucket_name = self.get_bucket_name(podcast)
+        update = self.connection.lookup(bucket_name) is not None
+        headers = self.get_bucket_headers(podcast, update=update)
+        return self.connection.create_bucket(bucket_name, headers=headers)
+
+    def get_key_name(self, blob):
+        return str(blob)  # Guaranteed to be unique
+
+    def get_or_create_key(self, blob):
+        bucket_name = self.get_bucket_name(blob.content_object)
+        bucket = Bucket(self.connection, bucket_name)
+        key_name = self.get_key_name(blob)
+        key = bucket.get_key(key_name)
+        if key is None:
+            key = bucket.new_key(key_name)
+        return key
+
+    def build_url(self, blob):
+        bucket_name = self.get_bucket_name(blob.content_object)
+        key_name = self.get_key_name(blob)
+        return 'https://archive.org/download/{bucket}/{key}'.format(bucket=quote(bucket_name), key=quote(key_name))
--- antxetamedia/blobs/tasks.py
+++ antxetamedia/blobs/tasks.py
@@ -1,62 +1,49 @@
-try:
-    from urllib.parse import quote
-except ImportError:
-    from urllib import quote
 import traceback
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from boto.s3.connection import S3Connection, OrdinaryCallingFormat
-from boto.exception import S3ResponseError
 from celery import shared_task
 
+from ..shows.models import AbstractPodcast
 from .models import Blob, BlobUpload
+from .archive import ArchiveS3
+
+
+RETRY_POLICY = {
+    'max_retries': 5,
+    'interval_start': 0,
+    'interval_step': 5*60,
+    'interval_max': 60*60,
+}
 
 
 @receiver(post_save, sender=Blob)
 def queue_blob_upload(sender, instance, **kwargs):
-    if instance.local:
-        upload_blob.apply_async([instance], retry_policy={
-            'max_retries': 5,
-            'interval_start': 0,
-            'interval_step': 5*60,
-            'interval_max': 60*60,
-        })
+    if instance.local:  # Only sync if there is something to upload
+        update_blob.apply_async([instance], retry_policy=RETRY_POLICY)
+
+
+def queue_podcast_update(sender, instance, **kwargs):
+    for blob in instance.blob_set.iterator():
+        update_blob.apply_async([blob], retry_policy=RETRY_POLICY)
+
+for subclass in AbstractPodcast.__subclasses__():
+    post_save.connect(queue_podcast_update, sender=subclass)
 
 
 @shared_task
-def upload_blob(blob):
-    if blob.local:
-        upload = BlobUpload.objects.create(blob=blob)
-        upload.is_uploading()
-
-        try:
-            # Create connection
-            connection = S3Connection(blob.account.username, blob.account.password, is_secure=False,
-                                      host='s3.us.archive.org', calling_format=OrdinaryCallingFormat())
-
-            # Get or create bucket
-            bucket = '{prefix}-{show}-{podcast}'.format(prefix=connection.access_key.lower(),
-                                                        show=blob.content_object.show.slug.lower(),
-                                                        podcast=blob.content_object.slug.lower())
-            try:
-                bucket = connection.get_bucket(bucket)
-            except S3ResponseError as exp:
-                if not exp.status == 404:
-                    raise
-                metadata = getattr(blob.content_object, 'metadata', {})
-                bucket = connection.create_bucket(bucket, headers=metadata)
-
-            # Create key
-            key = bucket.new_key(str(blob))  # Guaranteed to be unique
-
-            # Upload contents
+def update_blob(blob):
+    upload = BlobUpload.objects.create(blob=blob)
+    upload.is_uploading()
+    try:
+        connection = ArchiveS3(blob.account)
+        connection.create_or_update_bucket(blob.content_object)
+        key = connection.get_or_create_key(blob)
+        if blob.local:
             key.set_contents_from_file(blob.local.file)
-
-            # Build remote url
-            url = 'https://archive.org/download/{bucket}/{key}'.format(bucket=quote(bucket.name), key=quote(key.key))
-        except Exception as exp:
-            upload.is_unsuccessful(traceback.format_exc())
-        else:
-            upload.is_successful(url)
+        url = connection.build_url(blob)
+    except Exception:
+        upload.is_unsuccessful(traceback.format_exc())
+    else:
+        upload.is_successful(url)
