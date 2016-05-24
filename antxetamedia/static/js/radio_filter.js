7b853ce3e952c6645c8e584a1b0e5067f08d5359
Unai Zalakain <unai@gisa-elkartea.org>
2016-05-24 10:39:54 +0200

    Update radio filter selectors

--- antxetamedia/static/js/radio_filter.js
+++ antxetamedia/static/js/radio_filter.js
@@ -7,13 +7,12 @@
     var filter = this.name,
         value = this.value,
         checked = this.checked;
-    var selector = '#radioShows li.show[data-{filter}="{value}"]',
+    var selector = '#radioShows li[data-{filter}="{value}"]',
         tag = 'data-{filter}-is-hidden';
     selector = selector.replace('{filter}', filter);
     selector = selector.replace('{value}', value);
     tag = tag.replace('{filter}', filter);
     var shows = document.querySelectorAll(selector);
-    console.log(selector);
     Array.from(shows).forEach(function(show) {
       show.setAttribute(tag, !checked);
       if (show.getAttribute('data-producer-is-hidden') === "true" ||
