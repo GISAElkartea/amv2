ab44818bfb80142c0fb41a5d814faa4a2ae28288
Unai Zalakain <unai@gisa-elkartea.org>
2016-08-04 23:47:11 +0200

    Fix news migration pk reference issues

--- migrate.py
+++ migrate.py
@@ -24,24 +24,26 @@ def filter_model(data, model):
     return (obj for obj in data if obj['model'] == model)
 
 
-def import_news(objects):
-    # Import news categories
-
-    for nc in filter_model(objects, 'recordings.newscategory'):
-        if NewsCategory.objects.filter(slug=nc['fields']['slug']).exists():
-            logging.debug("Ignoring NewsCategory with slug {}.".format(nc['fields']['slug']))
-        else:
+def import_news_categories(news_categories):
+    pk_old_to_new = {}
+    for nc in news_categories:
+        try:
+            news_category = NewsCategory.objects.get(slug=nc['fields']['slug'])
+        except NewsCategory.DoesNotExist:
             logging.info("Importing NewsCategory with slug {}.".format(nc['fields']['slug']))
-            NewsCategory.objects.create(name=nc['fields']['name'],
-                                        slug=nc['fields']['slug'])
+            news_category = NewsCategory.objects.create(name=nc['fields']['name'],
+                                                        slug=nc['fields']['slug'])
+        else:
+            logging.debug("Ignoring NewsCategory with slug {}.".format(nc['fields']['slug']))
+        pk_old_to_new[nc['pk']] = news_category.pk
+    return pk_old_to_new
 
+
+def import_news_podcasts(news_podcasts, news_category_correspondence):
+    pk_old_to_new = {}
     default_show, _ = NewsShow.objects.get_or_create(name='Antxeta Irratia',
                                                      slug='antxeta-irratia')
-
-    # Import news podcasts
-
-    pk_old_to_new = {}
-    for np in filter_model(objects, 'recordings.news'):
+    for np in news_podcasts:
         try:
             news_podcast = NewsPodcast.objects.get(slug=np['fields']['slug'])
         except NewsPodcast.DoesNotExist:
@@ -54,31 +56,50 @@ def import_news(objects):
                                                       show=default_show)
         else:
             logging.debug("Ignoring NewsPodcast with slug {}.".format(np['fields']['slug']))
-        news_podcast.categories = np['fields']['categories']
+
+        news_podcast.categories = [news_category_correspondence[pk] for pk in np['fields']['categories']]
         pk_old_to_new[np['pk']] = news_podcast.pk
 
-    # Import news blobs
+    return pk_old_to_new
 
-    news_content_type = ContentType.objects.get_for_model(NewsPodcast)
+
+def import_news_blobs(news_blobs, news_podcast_correspondence):
+    news_podcast_ct = ContentType.objects.get_for_model(NewsPodcast)
     account = Account.objects.get(name=ACCOUNT_NAME)
 
-    for b in filter_model(objects, 'multimedia.media'):
-        if all((b['fields']['content_type'] == ['recordings', 'news'],
-                b['fields']['remote'] and b['fields']['remote'].startswith('http'))):
-            if Blob.objects.filter(remote=b['fields']['remote']).exists():
-                logging.debug("Ignoring Blob with remote {}.".format(b['fields']['remote']))
-            else:
-                logging.info("Importing Blob with remote {}.".format(b['fields']['remote']))
-                siblings = Blob.objects.filter(content_type=news_content_type,
-                                               object_id=pk_old_to_new[b['fields']['object_id']])
-                position = siblings.aggregate(position=Max('position'))['position']
-                position = 0 if position is None else position + 1
+    pk_old_to_new = {}
+    for nb in news_blobs:
+        try:
+            news_blob = Blob.objects.get(remote=nb['fields']['remote'])
+        except Blob.DoesNotExist:
+            logging.info("Importing Blob with remote {}.".format(nb['fields']['remote']))
+            siblings = Blob.objects.filter(content_type=news_podcast_ct,
+                                           object_id=news_podcast_correspondence[nb['fields']['object_id']])
+            position = siblings.aggregate(position=Max('position'))['position']
+            position = 0 if position is None else position + 1
 
-                Blob.objects.create(content_type=news_content_type,
-                                    object_id=pk_old_to_new[b['fields']['object_id']],
-                                    position=position,
-                                    remote=b['fields']['remote'],
-                                    account=account)
+            news_blob = Blob.objects.create(content_type=news_podcast_ct,
+                                            object_id=news_podcast_correspondence[nb['fields']['object_id']],
+                                            position=position,
+                                            remote=nb['fields']['remote'],
+                                            account=account)
+        else:
+            logging.debug("Ignoring Blob with remote {}.".format(nb['fields']['remote']))
+
+        pk_old_to_new[nb['pk']] = news_blob.pk
+    return pk_old_to_new
+
+
+def import_news(objects):
+    nc = filter_model(objects, 'recordings.newscategory')
+    nc_correspondence = import_news_categories(nc)
+    np = filter_model(objects, 'recordings.news')
+    np_correspondence = import_news_podcasts(np, nc_correspondence)
+    nb = filter_model(objects, 'multimedia.media')
+    nb = (b for b in nb if (b['fields']['content_type'] == ['recordings', 'news'] and
+                            b['fields']['remote'] and
+                            b['fields']['remote'].startswith('http')))
+    import_news_blobs(nb, np_correspondence)
 
 
 def import_radio(objects):
