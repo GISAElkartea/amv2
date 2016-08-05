c397ac7841d2c6153e936292a3651873d7c8a0ae
Unai Zalakain <unai@gisa-elkartea.org>
2016-08-05 18:19:42 +0200

    Add support for project migration

--- migrate.py
+++ migrate.py
@@ -9,12 +9,14 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "antxetamedia.settings")
 django.setup()
 
+from django.apps import apps
 from django.db.models import Max
 from django.utils.timezone import make_aware
 from django.contrib.contenttypes.models import ContentType
 
 from antxetamedia.news.models import NewsCategory, NewsPodcast, NewsShow
 from antxetamedia.blobs.models import Blob, Account
+from antxetamedia.projects.models import ProjectProducer, ProjectShow, ProjectPodcast
 
 
 ACCOUNT_NAME = 'archive.org'
@@ -22,6 +24,37 @@ ACCOUNT_NAME = 'archive.org'
 
 def filter_model(data, model):
     return (obj for obj in data if obj['model'] == model)
+
+
+def filter_blobs(data, content_type):
+    return (obj for obj in data if (obj['model'] == 'multimedia.media' and
+                                    obj['fields']['content_type'] == content_type.split('.') and
+                                    obj['fields']['remote'] and
+                                    obj['fields']['remote'].startswith('http')))
+
+
+def import_blobs(blobs, content_type, correspondence):
+    content_type = ContentType.objects.get_for_model(apps.get_model(content_type))
+    account = Account.objects.get(name=ACCOUNT_NAME)
+
+    pk_old_to_new = {}
+    for b in blobs:
+        remote = b['fields']['remote']
+        try:
+            blob = Blob.objects.get(remote=remote)
+        except Blob.DoesNotExist:
+            logging.info("Importing Blob with remote {}.".format(remote))
+            object_id = correspondence[b['fields']['object_id']]
+            siblings = Blob.objects.filter(content_type=content_type, object_id=object_id)
+            position = siblings.aggregate(position=Max('position'))['position']
+            position = 0 if position is None else position + 1
+            blob = Blob.objects.create(content_type=content_type, object_id=object_id, position=position,
+                                       remote=remote, account=account)
+        else:
+            logging.debug("Ignoring Blob with remote {}.".format(remote))
+
+        pk_old_to_new[b['pk']] = blob.pk
+    return pk_old_to_new
 
 
 def import_news_categories(news_categories):
@@ -63,43 +96,58 @@ def import_news_podcasts(news_podcasts, news_category_correspondence):
     return pk_old_to_new
 
 
-def import_news_blobs(news_blobs, news_podcast_correspondence):
-    news_podcast_ct = ContentType.objects.get_for_model(NewsPodcast)
-    account = Account.objects.get(name=ACCOUNT_NAME)
-
-    pk_old_to_new = {}
-    for nb in news_blobs:
-        try:
-            news_blob = Blob.objects.get(remote=nb['fields']['remote'])
-        except Blob.DoesNotExist:
-            logging.info("Importing Blob with remote {}.".format(nb['fields']['remote']))
-            siblings = Blob.objects.filter(content_type=news_podcast_ct,
-                                           object_id=news_podcast_correspondence[nb['fields']['object_id']])
-            position = siblings.aggregate(position=Max('position'))['position']
-            position = 0 if position is None else position + 1
-
-            news_blob = Blob.objects.create(content_type=news_podcast_ct,
-                                            object_id=news_podcast_correspondence[nb['fields']['object_id']],
-                                            position=position,
-                                            remote=nb['fields']['remote'],
-                                            account=account)
-        else:
-            logging.debug("Ignoring Blob with remote {}.".format(nb['fields']['remote']))
-
-        pk_old_to_new[nb['pk']] = news_blob.pk
-    return pk_old_to_new
-
-
 def import_news(objects):
     nc = filter_model(objects, 'recordings.newscategory')
     nc_correspondence = import_news_categories(nc)
     np = filter_model(objects, 'recordings.news')
     np_correspondence = import_news_podcasts(np, nc_correspondence)
-    nb = filter_model(objects, 'multimedia.media')
-    nb = (b for b in nb if (b['fields']['content_type'] == ['recordings', 'news'] and
-                            b['fields']['remote'] and
-                            b['fields']['remote'].startswith('http')))
-    import_news_blobs(nb, np_correspondence)
+    nb = filter_blobs(objects, 'recordings.news')
+    import_blobs(nb, 'news.NewsPodcast', np_correspondence)
+
+
+def import_project_shows(project_shows):
+    default_producer, _ = ProjectProducer.objects.get_or_create(name='Antxeta Irratia',
+                                                                slug='antxeta-irratia')
+    pk_old_to_new = {}
+    for ps in project_shows:
+        try:
+            project_show = ProjectShow.objects.get(slug=ps['fields']['slug'])
+        except ProjectShow.DoesNotExist:
+            logging.info("Importing ProjectShow with slug {}.".format(ps['fields']['slug']))
+            project_show = ProjectShow.objects.create(name=ps['fields']['name'],
+                                                      slug=ps['fields']['slug'],
+                                                      producer=default_producer,
+                                                      creation_date=ps['fields']['beginning'],
+                                                      description=ps['fields']['text'],
+                                                      image=ps['fields']['image'])
+        else:
+            logging.debug("Ignoring ProjectShow with slug {}.".format(ps['fields']['slug']))
+        pk_old_to_new[ps['pk']] = project_show.pk
+    return pk_old_to_new
+
+
+def import_project_podcasts(project_blobs, project_show_correspondence):
+    pk_old_to_new = {}
+    for pb in project_blobs:
+        try:
+            project_podcast = ProjectPodcast.objects.get(title=pb['fields']['title'])
+        except ProjectPodcast.DoesNotExist:
+            logging.info("Importing ProjectPodcast with title {}.".format(pb['fields']['title']))
+            show_id = project_show_correspondence[pb['fields']['object_id']]
+            project_podcast = ProjectPodcast.objects.create(title=pb['fields']['title'],
+                                                            show=ProjectShow.objects.get(pk=show_id))
+        else:
+            logging.debug("Ignoring ProjectPodcast with title {}.".format(pb['fields']['title']))
+        pk_old_to_new[pb['fields']['object_id']] = project_podcast.pk
+    return pk_old_to_new
+
+
+def import_projects(objects):
+    ps = filter_model(objects, 'projects.project')
+    ps_correspondence = import_project_shows(ps)
+    pb = filter_blobs(objects, 'projects.project')
+    pp_correspondence = import_project_podcasts(pb, ps_correspondence)
+    import_blobs(pb, 'projects.ProjectPodcast', pp_correspondence)
 
 
 def import_radio(objects):
@@ -121,5 +169,7 @@ if __name__ == '__main__':
         sys.stderr.write('{} [-v] ([news] [radio] [projects] [agenda])\n'.format(sys.argv[0]))
     if 'news' in sys.argv[1:]:
         import_news(objects)
+    if 'projects' in sys.argv[1:]:
+        import_projects(objects)
     if 'radio' in sys.argv[1:]:
         import_radio(objects)
