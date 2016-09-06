f7ec6eaf65a2e973eed57e8a7eae58d1e527c455
Unai Zalakain <unai@gisa-elkartea.org>
2016-09-06 16:17:02 +0200

    Correctly get the default manager of a model instance

--- antxetamedia/news/fields.py
+++ antxetamedia/news/fields.py
@@ -5,6 +5,6 @@ class UniqueTrueBooleanField(BooleanField):
     def pre_save(self, model_instance, add):
         value = getattr(model_instance, self.attname)
         if value:
-            others = model_instance._default_manager.filter(**{self.attname: True})
+            others = model_instance._meta.default_manager.filter(**{self.attname: True})
             others.update(**{self.attname: False})
         return value
