14a5bccf2c2ba72c453ade8f9de15028bfda61a1
Unai Zalakain <unai@gisa-elkartea.org>
2016-09-26 21:18:19 +0100

    Explicitly do not disable existing loggers

--- antxetamedia/heroku/settings.py
+++ antxetamedia/heroku/settings.py
@@ -102,6 +102,7 @@ RAYGUN_API_KEY = os.getenv('RAYGUN_APIKEY')
 RAYGUN_API_ENABLED = True
 
 LOGGING = {
+    'disable_existing_loggers': False,
     'handlers': {
         'email-thumbnail': {
             'level': 'DEBUG',
